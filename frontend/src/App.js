442f40fe08544cd2b127c88b438cc8f438b3a255
Rabin-Kalikote <rabinkalikote33@gmail.com>
2024-04-13 20:53:21 -0600

    flask running

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [established, establish] = useState(0);
+
+  useEffect(() => {
+    fetch('/').then(res => res.json()).then(data => {
+      establish("Hello World!");
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        ... no changes in this part ...
+
+        <p>{established}</p>
       </header>
     </div>
   );
